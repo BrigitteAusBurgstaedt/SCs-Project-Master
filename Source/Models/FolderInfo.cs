b27a1e4f80f29e6d3418f0a1c14ae7f8025cf562
BrigitteAusBurgstaedt <robin260602@gmail.com>
2024-08-11 11:08:02 +0200

    Add Folder Classes

---
+++ Source/Models/FolderInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCsProjectMaster.Source.Models;
+
+internal class FolderInfo
+{
+    public string FolderName { get; set; }
+    public ICollection<FolderInfo> Children { get; set; } = new List<FolderInfo>();
+}
---
+++ Source/Models/FolderPreset.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCsProjectMaster.Source.Models;
+
+internal class FolderPreset
+{
+    public string Name { get; set; }
+    public FolderInfo Root { get; set; } = new();
+}
