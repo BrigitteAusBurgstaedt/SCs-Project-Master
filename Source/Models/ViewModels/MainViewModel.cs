f6e7aa4f3fbd617d9b2c8162419e6237d0b9feac
BrigitteAusBurgstaedt <robin260602@gmail.com>
2024-08-13 08:14:37 +0200

    Add Messages for Status and Error Feedback

--- Source/Models/ViewModels/MainViewModel.cs
+++ Source/Models/ViewModels/MainViewModel.cs
@@ -6,24 +6,38 @@ namespace SCsProjectMaster.Source.Models.ViewModels;
 
 internal partial class MainViewModel : ObservableObject
 {
+    public MessageViewModel Status { get; }
+    public MessageViewModel Error { get; }
+
     [ObservableProperty]
     private ObservableCollection<Project> _projects;
 
     [ObservableProperty]
     private Project _project = new();
 
+    [ObservableProperty]
+    private bool _hasProject = false;
+
     public MainViewModel() 
     {
+        Status = new MessageViewModel();
+        Error = new MessageViewModel();
+
         using DatabaseContext db = new();
         try
         {
             Projects = db.Projects.ToObservableCollection();
+            Status.Message = "Online";
         }
-        catch (Exception ex)
+        catch (Exception)
         {
-            Console.WriteLine(ex.ToString());
-            throw;
+            Error.Message = "Fehler beim Zugriff auf die Datenbank. Internetverbindung überprüfen!";
         }
+    }
+
+    partial void OnProjectChanged(Project value)
+    {
+        HasProject = (value.Name != "");
     }
 
 }
---
+++ Source/Models/ViewModels/MessageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SCsProjectMaster.Source.Models.ViewModels;
+
+internal partial class MessageViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _message;
+
+    [ObservableProperty]
+    private bool _hasMessage = false;
+
+    partial void OnMessageChanged(string value)
+    {
+        HasMessage = !string.IsNullOrEmpty(value);
+    }
+}
