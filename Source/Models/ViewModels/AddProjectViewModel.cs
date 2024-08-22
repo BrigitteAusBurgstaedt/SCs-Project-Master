8e864665c29d445d282c4d35eac120022dffc184
BrigitteAusBurgstaedt <robin260602@gmail.com>
2024-08-22 18:00:59 +0200

    added project view

---
+++ Source/Models/ViewModels/AddProjectViewModel.cs
@@ -0,0 +1,92 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+
+namespace SCsProjectMaster.Source.Models.ViewModels;
+
+internal partial class AddProjectViewModel : ObservableObject
+{
+    public MessageViewModel Status { get; }
+    public MessageViewModel Error { get; }
+
+    [ObservableProperty]
+    private Project _project = new();
+
+    [ObservableProperty]
+    private IList<Customer> _customers;
+
+    [ObservableProperty]
+    private Customer _selectedCustomer;
+
+    [ObservableProperty]
+    private IList<Employee> _employees;
+
+    [ObservableProperty]
+    private Employee _selectedEmployee;
+
+    [ObservableProperty]
+    private IList<Employee> _selectedEmployees;
+
+    [ObservableProperty]
+    private Configuration _configuration = Configuration.Instance;
+
+    [ObservableProperty]
+    private IList<string> _keys = Configuration.Instance.CategorieNamesAndPaths.Keys.ToList();
+
+    public AddProjectViewModel()
+    {
+        Status = new MessageViewModel();
+        Error = new MessageViewModel();
+
+        using DatabaseContext db = new();
+        try
+        {
+            Customers = db.Customers.ToList();
+            Employees = db.Employees.ToList();
+        }
+        catch (Exception)
+        {
+            Error.Message = "Fehler beim Zugriff auf die Datenbank. Internetverbindung überprüfen!";
+        }
+    }
+
+    [RelayCommand]
+    private void Add()
+    {
+        Error.Message = "";
+        Status.Message = "";
+
+        if (Project.Name == "")
+        {
+            Error.Message = "Fehler: Projektname darf nicht leer sein.";
+            return;
+        }
+
+        if (SelectedCustomer == null || SelectedEmployee == null)
+        {
+            Error.Message = "Fehler: Eingabe überprüfen. Kunde und Ansprechpartner dürfen nicht leer sein.";
+            return;
+        }
+
+        using DatabaseContext db = new();
+        try
+        {
+            Project.CustomerId = SelectedCustomer.Id;
+            Project.ContactPerson = SelectedEmployee.Login;
+            Project.EmployeeLogins.Add(SelectedEmployee);
+            foreach (Employee teamMember in SelectedEmployees)
+            {
+                if (!teamMember.Equals(SelectedEmployee))
+                    Project.EmployeeLogins.Add(teamMember);
+            }
+            db.Projects.Add(Project);
+            db.SaveChanges();
+            Status.Message = "Projekt hinzugefügt";
+            Project = new Project();
+        }
+        catch (Exception)
+        {
+            Error.Message = "Fehler: Zugriff auf die Datenbank nicht möglich. Internetverbindung überprüfen.";
+        }
+    }
+}
