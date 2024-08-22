805e7079c3af1ac1f07665e49f96c83d582c910b
BrigitteAusBurgstaedt <robin260602@gmail.com>
2024-08-22 17:59:46 +0200

    added status messages

--- Source/Models/ViewModels/AddCustomerViewModel.cs
+++ Source/Models/ViewModels/AddCustomerViewModel.cs
@@ -5,17 +5,38 @@ namespace SCsProjectMaster.Source.Models.ViewModels;
 
 internal partial class AddCustomerViewModel : ObservableObject
 {
+    public MessageViewModel Status { get; }
+    public MessageViewModel Error { get; }
+
     [ObservableProperty]
     private Address _address = new();
     [ObservableProperty]
     private Customer _customer = new();
 
+    public AddCustomerViewModel()
+    {
+        Error = new MessageViewModel();
+        Status = new MessageViewModel();
+    }
+
     [RelayCommand]
     private void SaveChanges()
     {
+        Error.Message = "";
+        Status.Message = "";
+
         using DatabaseContext db = new();
-        Customer.Address = Address;
-        db.Customers.Add(Customer);
-        db.SaveChanges();
+        try
+        {
+            Customer.Address = Address;
+            db.Customers.Add(Customer);
+            db.SaveChanges();
+            Status.Message = "Kunde hinzugefügt";
+        }
+        catch (Exception)
+        {
+            Error.Message = "Fehler: Zugriff auf die Datenbank nicht möglich. Internetverbindung überprüfen.";
+        }
+
     }
 }
