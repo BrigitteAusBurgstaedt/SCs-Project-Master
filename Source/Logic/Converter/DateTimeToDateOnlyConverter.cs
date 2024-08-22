57cdde411d5c04cfc60fa03c61c743b0eb54a2a4
BrigitteAusBurgstaedt <robin260602@gmail.com>
2024-08-22 17:58:08 +0200

    added converter

---
+++ Source/Logic/Converter/DateTimeToDateOnlyConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace SCsProjectMaster.Source.Logic.Converter;
+
+internal class DateOnlyToDateTimeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is DateOnly date)
+            return date.ToDateTime(TimeOnly.MinValue);
+
+        return DateTime.Now;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is DateTime dateTime)
+            return DateOnly.FromDateTime(dateTime);
+
+        return DateOnly.FromDateTime(DateTime.Now);
+    }
+}
