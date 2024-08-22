550870710f3fb81a87f842978280597a7dc79f9d
BrigitteAusBurgstaedt <robin260602@gmail.com>
2024-08-22 18:01:16 +0200

    added project view

---
+++ Source/Views/AddProjectView.xaml.cs
@@ -0,0 +1,12 @@
+using SCsProjectMaster.Source.Models.ViewModels;
+
+namespace SCsProjectMaster.Source.Views;
+
+public partial class AddProjectView : ContentPage
+{
+	public AddProjectView()
+	{
+		InitializeComponent();
+        BindingContext = new AddProjectViewModel();
+    }
+}
